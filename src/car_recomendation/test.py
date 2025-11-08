96d01c12c796bebf2a817cbba3c34b483f923b78
stchen2005 <stchen@vt.edu>
2025-11-08 16:14:13 -0500

    Update test.py

--- src/car_recomendation/test.py
+++ src/car_recomendation/test.py
@@ -3,5 +3,7 @@ print("test from desktop")
 
 import pandas as pd
 
-df = pd.read_csv("data/Cars Datasets 2025.csv")  # or whatever the filename is
+df = pd.read_csv("data/Cars Datasets 2025.csv", encoding="latin1")
 print(df.head())
+print(df.columns)
+print(df.info())
