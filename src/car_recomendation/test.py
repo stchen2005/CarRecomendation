a33172f33d2e86815c13ec8f08e51710f9d5e024
stchen2005 <stchen@vt.edu>
2025-11-15 19:05:41 -0500

    Update test.py

--- src/car_recomendation/test.py
+++ src/car_recomendation/test.py
@@ -2,8 +2,18 @@ print("test")
 print("test from desktop")
 
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
 
-df = pd.read_csv("data/Cars Datasets 2025.csv", encoding="latin1")
-print(df.head())
-print(df.columns)
-print(df.info())
+car_data = pd.read_csv("data/Cars Datasets 2025.csv", encoding="latin1")
+print(car_data.shape)
+print(car_data['Seats'].value_counts)
+encoder = LabelEncoder()
+car_data['Seats'] = encoder.fit_transform(car_data['Seats'])
+
+print(car_data.groupby("Seats").mean(numeric_only=True))
+print(car_data.columns)
+print(car_data.dtypes)
